940da0d67d859b17f21ffbcf5eb84f87ffebbf7a
xj <2932600521@qq.com>
2020-11-18 19:51:42 +0800

    idea的修改

--- src/test/test.java
+++ src/test/test.java
@@ -3,6 +3,6 @@ package test;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("11111111111");
+        System.out.println("11111111111asadsdsds");
     }
 }
