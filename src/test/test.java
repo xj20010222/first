9b55efb9a42fae4e25c6c1be6638249e87eaf801
xj20010222 <74606430+xj20010222@users.noreply.github.com>
2020-11-18 21:31:19 +0800

    Update test.java

--- src/test/test.java
+++ src/test/test.java
@@ -5,4 +5,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("11111111111asadsdsds");
     }
+    
+    public void fun1(){
+    System.out.println("11111111111111111111111111111111");
+    
+    
+    }
 }
